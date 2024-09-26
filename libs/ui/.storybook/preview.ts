72b0f2c7cc9099a95e33aecf5be0e87e3942e1d7
Óscar Víquez <hello@oscarviquez.com>
2024-09-26 13:55:08 -0600

    Font awesome setup

--- libs/ui/.storybook/preview.ts
+++ libs/ui/.storybook/preview.ts
@@ -1,18 +1,16 @@
 import '../src/styles/ui.scss'
 import type { Preview } from "@storybook/vue3";
-import '@fortawesome/fontawesome-free/css/all.css';
 
 const preview: Preview = {
-    parameters: {
-      controls: {
-        matchers: {
-          color: /(background|color)$/i,
-          date: /Date$/i,
-        },
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
-    tags: ['autodocs'],
-  };
-  
-  export default preview;
-  
+  },
+  tags: ['autodocs'],
+};
+
+export default preview;
--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,6 +1,3 @@
-import '@fortawesome/fontawesome-free/css/all.min.css';
-export { default as Dropdown } from './components/dropdown/dropdown.vue';
-export { default as TextWithIcon } from './components/text_with_icon/text_with_icon.vue';
 // Layouts
 export { default as MainLayout } from './layouts/main/main.vue';
 export { default as AuthLayout } from './layouts/auth/auth.vue';
@@ -10,3 +7,5 @@ export { default as Accordion } from './components/accordion/accordion.vue';
 export { default as GridItem } from './components/grid_item/grid_item.vue';
 export { default as Grid } from './components/grid/grid.vue';
 export { default as StyledButton } from './components/styled_button/styled_button.vue';
+export { default as Dropdown } from './components/dropdown/dropdown.vue';
+export { default as TextWithIcon } from './components/text_with_icon/text_with_icon.vue';

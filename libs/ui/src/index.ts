465f5ecf26804b5e62caab04957c7d593318e54c
marciacastillo <marcia.castillo@decimoinc.com>
2024-10-01 13:00:35 -0300

    add types to index ts

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,6 +1,3 @@
-export { default as PaymentCard } from './components/cards/payment-card/payment-card.vue';
-export { default as JobCard } from './components/cards/job-card/job-card.vue';
-export { default as FilePicker } from './components/filePicker/filePicker.vue';
 // Layouts
 export { default as MainLayout } from './layouts/main/main.vue';
 export { default as AuthLayout } from './layouts/auth/auth.vue';
@@ -10,7 +7,13 @@ export { default as SideMenu } from './components/side-menu/side-menu.vue';
 export { default as TopBar } from './components/top-bar/top-bar.vue';
 export { default as Dropdown } from './components/dropdown/dropdown.vue';
 export { default as DropdownCard } from './components/cards/dropdown-card/dropdown-card.vue';
+export { default as PaymentCard } from './components/cards/payment-card/payment-card.vue';
+export { default as JobCard } from './components/cards/job-card/job-card.vue';
+export { default as FilePicker } from './components/filePicker/filePicker.vue';
 
 // Types
 export * from './types/side-menu';
 export * from './types/dropdown';
+export * from './types/cards/job-card';
+export * from './types/cards/payment-card';
+export * from './types/cards/dropdown-card';
